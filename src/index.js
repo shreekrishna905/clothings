b971cc6729f276f0538c26a030397833919e3e0f
shree krishna <shreekrishna905@gmail.com>
2021-05-31 17:58:55 +0545

    redux added and implemented userReducer and userActions

--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
 
